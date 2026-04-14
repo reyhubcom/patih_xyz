4ea92a0cc43f53591298637ac116dba848fea315
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-04-13 21:24:28 -0400

    1

--- src/navigation.ts
+++ src/navigation.ts
@@ -76,7 +76,7 @@ export const headerData = {
     },
     {
       text: 'Wallet',
-      href: '#',
+      href: '/home/BUY',
     },
   ],
   actions: [{ text: 'EXCHANGE', href: '/home/exchange', target: '' }],
